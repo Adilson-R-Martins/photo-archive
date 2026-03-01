05707979c9290325cd5689b0fef5db957ccc3044
Adilson-R-Martins <adilson.martins@protonmail.com>
2026-03-01 18:24:07 -0300

    chore: initial project setup with Spring Boot and dependencies

---
+++ src/main/java/br/com/cameraeluz/acervo/PhotoArchiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.cameraeluz.acervo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cameraeluz/acervo/PhotoArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cameraeluz.acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
