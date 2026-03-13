7ee83036658285e1e167e23d761d64037d7af781
Adilson-R-Martins <adilson.martins@protonmail.com>
2026-03-13 20:46:41 -0300

    fix: removed @NotBlank validation

--- src/main/java/br/com/cameraeluz/acervo/dto/PhotoUpdateDTO.java
+++ src/main/java/br/com/cameraeluz/acervo/dto/PhotoUpdateDTO.java
@@ -1,16 +1,14 @@
 package br.com.cameraeluz.acervo.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
 import java.util.Set;
 
 @Data
 public class PhotoUpdateDTO {
 
-    @NotBlank(message = "O título não pode ser vazio")
     private String title;
     private String artisticAuthorName;
-    @NotBlank(message = "A categoria não pode ser vazia")
     private Set<Long> categoryIds;
     private Boolean active; // Use Boolean para aceitar nulo caso não queira mudar o status
 }
